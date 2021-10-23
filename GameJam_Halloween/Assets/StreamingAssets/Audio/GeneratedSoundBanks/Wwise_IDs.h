e1b2798b44ce12e0553ad0bd23c9a6c04ca902d0
tpg <terryperezgervais@gmail.com>
2021-10-23 12:40:42 +0200

    [ADD] Menu music

--- GameJam_Halloween/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ GameJam_Halloween/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,12 +13,14 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID MUSIC_MENU = 1598298728U;
         static const AkUniqueID TEST_MUSIC = 379521629U;
     } // namespace EVENTS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SNB_MENU = 761484738U;
         static const AkUniqueID SNB_TEST = 2162288179U;
     } // namespace BANKS
 
